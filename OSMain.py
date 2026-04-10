479101e8e2b7bef040f7cc50d110b768274d1367
Grade5-CodeDev <leogifted@naver.com>
2026-04-10 21:17:26 +0900

    Implement echo method and command input loop
    Added echo method to OS class and command input handling.

--- OSMain.py
+++ OSMain.py
@@ -1,6 +1,14 @@
 class OS:
   def boot(self):
     print("boot test")
+  def echo(self, text):
+    print(text)
 
 os = OS()
 os.boot()
+import re
+p_echo = re.compile("echo\s(.*)", re.DOTALL)
+while True:
+  choice = input("cmd >>> ")
+  if p_echo.match(choice):
+    os.echo(p_echo.sub("\g<1>"))
